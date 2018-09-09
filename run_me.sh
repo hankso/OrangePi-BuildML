6d7b1aad4dd4e3805b1752d8826c9121a8642ce0
hank <3080863354@qq.com>
2018-09-09 22:43:32 +0800

    update requirement.txt

--- run_me.sh
+++ run_me.sh
@@ -8,7 +8,7 @@ sudo nmtui
 
 
 
-srcfiledir=https://raw.githubusercontent.com/hankso/OrangePi-BuildML/master/files
+srcfiledir=https://raw.githubusercontent.com/hankso/OrangePi-BuildML/master
 dotfiledir=https://raw.githubusercontent.com/hankso/dotfiles/master
 
 
@@ -35,18 +35,12 @@ sudo apt-get update && sudo apt-get upgrade
 
 # install pip and many packages by apt & pip
 cd /tmp
-wget https://bootstrap.pypa.io/get-pip.py            && sudo python2 get-pip.py
-wget ${srcfiledir}/apt-install.list                  && sudo apt-get install -y $(cat apt-install.list 2> /dev/null)
-
-sudo pip install ${srcfiledir}/tensorflow-1.2.1-cp27-cp27mu-linux_aarch64.whl \
-                 ${srcfiledir}/scipy-1.2.0.dev0+994e74b-cp27-cp27mu-linux_aarch64.whl \
-                 ${srcfiledir}/reportlab-3.5.7-cp27-cp27mu-linux_aarch64.whl
-
-wget ${srcfiledir}/requirements.txt                  && sudo pip install -r requirements.txt 2> /dev/null
-wget ${srcfiledir}/liblsl64.so.1.4.0                 && sudo cp liblsl64.so.1.4.0 /usr/local/lib/python2.7/dist-packages/pylsl/liblsl64.so
+wget https://bootstrap.pypa.io/get-pip.py  && sudo python2 get-pip.py
+wget ${srcfiledir}/files/apt-install.list  && sudo apt-get install -y $(cat apt-install.list 2> /dev/null)
+sudo pip install tensorflow scipy pillow reportlab pylsl -i ${srcfiledir}/wheels
+wget ${srcfiledir}/files/requirements.txt  && sudo pip install -r requirements.txt 2> /dev/null
 
 cd ~
-
 
 
 # Tensorflow is so so different from other packages, it's hard to install
@@ -63,11 +57,3 @@ rm ngrok-stable-linux-arm.zip
 chmod +x ngrok
 sudo ln -sf $(pwd)/ngrok /usr/local/bin/ngrok
 
-
-# NodeJS is useful for me, if you don't need it just comment these lines
-# wget https://nodejs.org/dist/v9.6.1/node-v9.6.1-linux-arm64.tar.gz
-# tar xzf node-v9.6.1-linux-arm64.tar.gz
-# rm node-v9.6.1-linux-arm64.tar.gz
-# cd node-v9.6.1-linux-arm64/bin
-# sudo ln -sf $(pwd)/node /usr/local/bin/node
-# sudo ln -sf $(pwd)/npm /usr/local/bin/npm
