cfe54711025c283ac346d415a58084bb653a2eea
hank <3080863354@qq.com>
2018-09-09 18:17:55 +0800

    rename wheels

--- run_me.sh
+++ run_me.sh
@@ -3,17 +3,19 @@
 
 # *****only for python 2.7*****
 
-srcfiledir=https://raw.githubusercontent.com/hankso/OrangePi-BuildML/master/files
-dotfiledir=https://raw.githubusercontent.com/hankso/dotfiles/master
-
 # network
 sudo nmtui
 
 
 
+srcfiledir=https://raw.githubusercontent.com/hankso/OrangePi-BuildML/master/files
+dotfiledir=https://raw.githubusercontent.com/hankso/dotfiles/master
+
+
+
 # dirs
 cd ~
-mkdir -p document download programs notebook .pip .vim/bundle .vim/dirs/backups 
+mkdir -p document notebook .pip .vim/bundle .vim/dirs/backups 
 
 
 
@@ -35,10 +37,14 @@ sudo apt-get update && sudo apt-get upgrade
 cd /tmp
 wget https://bootstrap.pypa.io/get-pip.py            && sudo python2 get-pip.py
 wget ${srcfiledir}/apt-install.list                  && sudo apt-get install -y $(cat apt-install.list 2> /dev/null)
-wget ${srcfiledir}/tensorflow-1.2.1-cp27-aarch64.whl && sudo pip install ./tensorflow-1.2.1-cp27-aarch64.whl
-wget ${srcfiledir}/scipy-1.2.0-cp27-aarch64.whl      && sudo pip install ./scipy-1.2.0-cp27-aarch64.whl
+
+sudo pip install ${srcfiledir}/tensorflow-1.2.1-cp27-cp27mu-linux_aarch64.whl \
+                 ${srcfiledir}/scipy-1.2.0.dev0+994e74b-cp27-cp27mu-linux_aarch64.whl \
+                 ${srcfiledir}/reportlab-3.5.7-cp27-cp27mu-linux_aarch64.whl
+
 wget ${srcfiledir}/requirements.txt                  && sudo pip install -r requirements.txt 2> /dev/null
 wget ${srcfiledir}/liblsl64.so.1.4.0                 && sudo cp liblsl64.so.1.4.0 /usr/local/lib/python2.7/dist-packages/pylsl/liblsl64.so
+
 cd ~
 
 
@@ -50,7 +56,7 @@ cd ~
 
 
 # nice tool for accessing your pi in different LAN
-cd ~/programs
+cd ~/document
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
 rm ngrok-stable-linux-arm.zip
@@ -59,9 +65,9 @@ sudo ln -sf $(pwd)/ngrok /usr/local/bin/ngrok
 
 
 # NodeJS is useful for me, if you don't need it just comment these lines
-wget https://nodejs.org/dist/v9.6.1/node-v9.6.1-linux-arm64.tar.gz
-tar xzf node-v9.6.1-linux-arm64.tar.gz
-rm node-v9.6.1-linux-arm64.tar.gz
-cd node-v9.6.1-linux-arm64/bin
-sudo ln -sf $(pwd)/node /usr/local/bin/node
-sudo ln -sf $(pwd)/npm /usr/local/bin/npm
+# wget https://nodejs.org/dist/v9.6.1/node-v9.6.1-linux-arm64.tar.gz
+# tar xzf node-v9.6.1-linux-arm64.tar.gz
+# rm node-v9.6.1-linux-arm64.tar.gz
+# cd node-v9.6.1-linux-arm64/bin
+# sudo ln -sf $(pwd)/node /usr/local/bin/node
+# sudo ln -sf $(pwd)/npm /usr/local/bin/npm
